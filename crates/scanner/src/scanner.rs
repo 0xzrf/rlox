7a40f55a2f65ee590ba9e1772cf49e49da52bb95
Zeref <zeref18.drgnl@gmail.com>
2026-04-09 03:04:49 +0530

    added equal and equal equal update

--- crates/scanner/src/scanner.rs
+++ crates/scanner/src/scanner.rs
@@ -36,14 +36,19 @@ impl Scanner {
             let mut line_peekable = lines.char_indices().peekable();
 
             while let Some((ix, c)) = line_peekable.peek() {
-                let Some(token) = Self::get_token(c, ix) else {
+                let rest_of_line = lines.chars().skip(*ix + 1).collect::<String>(); // skip to ix + 1 so that rest_of_line.peak() gives the element after the current value
+
+                let Some((token, to_skip)) = Self::get_token(c, ix, rest_of_line) else {
                     eprintln!("[line {}] Error: Unexpected character: {}", line_ix + 1, c);
                     line_peekable.next();
                     exit_code = 65;
                     continue;
                 };
                 self.add_token(token);
-                line_peekable.next();
+
+                for _ in 0..to_skip {
+                    line_peekable.next();
+                }
             }
         }
 
@@ -53,23 +58,33 @@ impl Scanner {
         Ok(exit_code)
     }
 
-    pub fn get_token(c: &char, ix: &usize) -> Option<Token> {
-        let token_ty = match c {
-            '(' => TokenType::LEFT_PAREN,
-            ')' => TokenType::RIGHT_PAREN,
-            '{' => TokenType::LEFT_BRACE,
-            '}' => TokenType::RIGHT_BRACE,
-            ',' => TokenType::COMMA,
-            '.' => TokenType::DOT,
-            '-' => TokenType::MINUS,
-            '+' => TokenType::PLUS,
-            ';' => TokenType::SEMICOLON,
-            '*' => TokenType::STAR,
+    pub fn get_token(c: &char, start: &usize, rest_of_line: String) -> Option<(Token, usize)> {
+        let mut rest_peekable = rest_of_line.chars().peekable();
+
+        let (token_ty, lexeam, to_skip) = match c {
+            '(' => (TokenType::LEFT_PAREN, "(", 1),
+            ')' => (TokenType::RIGHT_PAREN, ")", 1),
+            '{' => (TokenType::LEFT_BRACE, "{", 1),
+            '}' => (TokenType::RIGHT_BRACE, "}", 1),
+            ',' => (TokenType::COMMA, ",", 1),
+            '.' => (TokenType::DOT, ".", 1),
+            '-' => (TokenType::MINUS, "-", 1),
+            '+' => (TokenType::PLUS, "+", 1),
+            ';' => (TokenType::SEMICOLON, ";", 1),
+            '*' => (TokenType::STAR, "*", 1),
+            '=' => {
+                if rest_peekable.peek() == Some(&'=') {
+                    (TokenType::EQUAL_EQUAL, "==", 2)
+                } else {
+                    (TokenType::EQUAL, "=", 1)
+                }
+            }
+
             _ => return None,
         };
 
 
-        Some(Token::new(token_ty, *ix, c.to_string()))
+        Some((Token::new(token_ty, *start, lexeam.to_string()), to_skip))
     }
 
     pub fn print_tokens(&self) {
