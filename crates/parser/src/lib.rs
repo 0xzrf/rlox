0c8d013aa56562207c5fbf82a73db2281e87bf5e
Zeref <zeref18.drgnl@gmail.com>
2026-04-16 23:28:41 +0530

    added tests

--- crates/parser/src/lib.rs
+++ crates/parser/src/lib.rs
@@ -2,4 +2,6 @@ mod parser;
 pub use parser::Parser;
 mod ast;
 mod parser_errors;
-pub use ast::AstPrinter;
+
+pub use ast::*;
+pub use parser_errors::ParserResult;
--- crates/parser/tests/lib.rs
+++ crates/parser/tests/lib.rs
@@ -1,9 +1,22 @@
 #[cfg(test)]
 pub mod parser_tests {
-    use parser::{AstPrinter, Parser};
+    use parser::{AstPrinter, Expr, Parser, ParserResult};
     use scanner::Scanner;
 
+    fn get_parse_result(source_code: &str) -> ParserResult<Expr> {
+        let tokens = Scanner::_new(source_code.to_string()).scan(false).unwrap().0.get_tokens();
+
+        Parser::new(&tokens).parse()
+    }
+
+    fn parse_code_and_return_ast(source_code: &str) -> Option<String> {
+        let parsed_expr = get_parse_result(source_code).ok()?;
+
+        Some(AstPrinter::print(&parsed_expr))
+    }
+
     #[test]
+    // #[ignore = "internal test function"]
     fn test_syntax_tree() {
         let source_code = "../../test.lox";
 
@@ -18,5 +31,14 @@ pub mod parser_tests {
         );
 
         println!("{:#?}", AstPrinter::print(&parser_result.unwrap()));
+    }
+
+    fn test_parses_binary() {
+        let source_code = "2 + 3";
+
+        let parse_result = parse_code_and_return_ast(source_code);
+
+        assert!(parse_result.is_some(), "expected the parser to output a value for the lox code");
+        assert_eq!(parse_result.unwrap(), "\"(+ 2.0 3.0)\"", "The parsed value doesn't match");
     }
 }
